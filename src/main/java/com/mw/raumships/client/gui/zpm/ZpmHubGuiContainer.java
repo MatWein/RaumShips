feee01f07f5d4a852db6869d5d65076e741699ae
Matthias Weinert <matthiasweinert@web.de>
2018-12-18 18:22:44 +0100

    .

--- src/main/java/com/mw/raumships/client/gui/zpm/ZpmHubGuiContainer.java
+++ src/main/java/com/mw/raumships/client/gui/zpm/ZpmHubGuiContainer.java
@@ -3,6 +3,7 @@ package com.mw.raumships.client.gui.zpm;
 import com.mw.raumships.RaumShipsMod;
 import com.mw.raumships.common.blocks.ZPMHubTileEntity;
 import com.mw.raumships.common.calc.PowerUnitCalculator;
+import com.mw.raumships.common.items.ZPMItem;
 import com.mw.raumships.server.gui.zpm.ZpmHubContainer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -22,8 +23,8 @@ public class ZpmHubGuiContainer extends GuiContainer {
     private static final ResourceLocation GUI_TEXTURE = new ResourceLocation(RaumShipsMod.MODID, "textures/gui/zpmhub.png");
     private static final NumberFormat NUMBER_FORMAT = NumberFormat.getNumberInstance(Locale.getDefault());
 
-    private static final int FIRST_LINE_X = 20 + ZpmHubContainer.SLOT_X_SPACING;
-    private static final int FIRST_LINE_Y = 10;
+    private static final int FIRST_LINE_X = 11 + ZpmHubContainer.SLOT_X_SPACING;
+    private static final int FIRST_LINE_Y = 7;
     private static final int FONT_COLOR = 0x000000;
 
     private final ZPMHubTileEntity zpmHubTileEntity;
@@ -39,6 +40,13 @@ public class ZpmHubGuiContainer extends GuiContainer {
     }
 
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        this.renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         Minecraft.getMinecraft().getTextureManager().bindTexture(GUI_TEXTURE);
 
@@ -48,30 +56,40 @@ public class ZpmHubGuiContainer extends GuiContainer {
 
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-        GL11.glPushMatrix();
-        GL11.glScalef(0.75F, 0.75F, 0.75F);
+        boolean before = fontRenderer.getUnicodeFlag();
+        try {
+            fontRenderer.setUnicodeFlag(true);
 
-        if (!zpmHubTileEntity.hasZpm()) {
-            fontRenderer.drawString(textNoZpm, FIRST_LINE_X, FIRST_LINE_Y, FONT_COLOR);
-        } else {
-            fontRenderer.drawString(textZpmInstalled, FIRST_LINE_X, FIRST_LINE_Y, FONT_COLOR);
+            if (!zpmHubTileEntity.hasZpm()) {
+                fontRenderer.drawString(textNoZpm, FIRST_LINE_X, FIRST_LINE_Y, FONT_COLOR);
+            } else {
+                fontRenderer.drawString(textZpmInstalled, FIRST_LINE_X, FIRST_LINE_Y, FONT_COLOR);
 
-            int rf = zpmHubTileEntity.getEnergyStored();
-            fontRenderer.drawString("FE / RF / T: " + NUMBER_FORMAT.format(rf), FIRST_LINE_X, FIRST_LINE_Y + 25, FONT_COLOR);
+                int rf = zpmHubTileEntity.getEnergyStored();
+                fontRenderer.drawString("FE / RF / T: " + NUMBER_FORMAT.format(rf), FIRST_LINE_X, FIRST_LINE_Y + 20, FONT_COLOR);
 
-            double eu = PowerUnitCalculator.toEU(rf);
-            fontRenderer.drawString("EU: " + NUMBER_FORMAT.format(eu), FIRST_LINE_X, FIRST_LINE_Y + 40, FONT_COLOR);
+                double eu = PowerUnitCalculator.toEU(rf);
+                fontRenderer.drawString("EU: " + NUMBER_FORMAT.format(eu), FIRST_LINE_X, FIRST_LINE_Y + 30, FONT_COLOR);
 
-            double j = PowerUnitCalculator.toJ(rf);
-            fontRenderer.drawString("J: " + NUMBER_FORMAT.format(j), FIRST_LINE_X, FIRST_LINE_Y + 55, FONT_COLOR);
+                double j = PowerUnitCalculator.toJ(rf);
+                fontRenderer.drawString("J: " + NUMBER_FORMAT.format(j), FIRST_LINE_X, FIRST_LINE_Y + 40, FONT_COLOR);
 
-            double mj = PowerUnitCalculator.toMJ(rf);
-            fontRenderer.drawString("MJ: " + NUMBER_FORMAT.format(mj), FIRST_LINE_X, FIRST_LINE_Y + 70, FONT_COLOR);
+                double mj = PowerUnitCalculator.toMJ(rf);
+                fontRenderer.drawString("MJ: " + NUMBER_FORMAT.format(mj), FIRST_LINE_X, FIRST_LINE_Y + 50, FONT_COLOR);
 
-            double gj = PowerUnitCalculator.togJ(rf);
-            fontRenderer.drawString("gJ: " + NUMBER_FORMAT.format(gj), FIRST_LINE_X, FIRST_LINE_Y + 85, FONT_COLOR);
+                double gj = PowerUnitCalculator.togJ(rf);
+                fontRenderer.drawString("gJ: " + NUMBER_FORMAT.format(gj), FIRST_LINE_X, FIRST_LINE_Y + 60, FONT_COLOR);
+
+                GL11.glPushMatrix();
+                GL11.glScalef(1.5F, 1.5F, 1.5F);
+
+                int percentage = (int)(rf * 100.0 / ZPMItem.MAX_ZPM_ENERGY);
+                fontRenderer.drawStringWithShadow(percentage + " %", FIRST_LINE_X + 63, FIRST_LINE_Y + 39, FONT_COLOR);
+
+                GL11.glPopMatrix();
+            }
+        } finally {
+            fontRenderer.setUnicodeFlag(before);
         }
-
-        GL11.glPopMatrix();
     }
 }
